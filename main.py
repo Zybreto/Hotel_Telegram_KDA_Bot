692c07981f0f0fb2dcdd6a63cd5c38eac88853e5
MbIUIb <kirill.chezlov@gmail.com>
2022-10-06 22:12:42 +0300

    add bot framework

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from aiogram import Bot, Dispatcher, executor, types
 from time import time, ctime
 
 from database import *
+
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)  # Dispatcher осуществляет анализ и обработку входящих обновлений бота
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
