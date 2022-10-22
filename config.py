fda5b7aa972eb7a43b046573813e2a028e4535be
MbIUIb <kirill.chezlov@gmail.com>
2022-10-22 15:36:21 +0300

    clear spam

--- config.py
+++ config.py
@@ -16,27 +16,3 @@ DESCRIPTION = """
 ля ля
 ля"""
 
-users_columns = 'tg_id INTEGER,' \
-                'surname TEXT,' \
-                'name TEXT,' \
-                'patronymic TEXT'
-
-bank_cards_columns = 'tg_id INTEGER,' \
-                     'card_number INTEGER,' \
-                     'holders_name TEXT,' \
-                     'validity_month INTEGER,' \
-                     'validity_year INTEGER,' \
-                     'cvv INTEGER'
-
-room_condition_columns = 'room_id INTEGER,' \
-                          'occupancy_status INTEGER,' \
-                          'entry_date timestamp,' \
-                          'departure_date timestamp'
-
-room_characteristics_columns = 'room_id INTEGER,' \
-                               'room_type TEXT,' \
-                               'room_cost INTEGER,' \
-                               'single_beds_num INTEGER,' \
-                               'double_beds_num INTEGER,' \
-                               'sofas_num INTEGER,' \
-                               'additions TEXT'
